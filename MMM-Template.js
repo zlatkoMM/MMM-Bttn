c0058810f8dd0a136d4900fdbee62b7021f7e726
zlatkoMM <magicmirror.zlatko@gmail.com>
2024-11-06 14:42:32 +0100

    Update MMM-Bttn.js

--- MMM-Template.js
+++ MMM-Template.js
@@ -1,64 +1,119 @@
-Module.register("MMM-Template", {
+/* global Module */
 
-  defaults: {
-    exampleContent: ""
-  },
+/* Magic Mirror
+ * Module: Bttn
+ *
+ * By zlatkoMM
+ * MIT Licensed.
+ */
 
-  /**
-   * Apply the default styles.
-   */
-  getStyles() {
-    return ["template.css"]
-  },
+Module.register("MMM-Bttn", {
 
-  /**
-   * Pseudo-constructor for our module. Initialize stuff here.
-   */
-  start() {
-    this.templateContent = this.config.exampleContent
+    requiresVersion: "2.1.0",
 
-    // set timeout for next random text
-    setInterval(() => this.addRandomText(), 3000)
-  },
+    // Default module config.
+    defaults: {
+        buttons: [],
+        minPressTime: 0,
+        maxPressTime: 500,
+        bounceTimeout: 300
+    },
 
-  /**
-   * Handle notifications received by the node helper.
-   * So we can communicate between the node helper and the module.
-   *
-   * @param {string} notification - The notification identifier.
-   * @param {any} payload - The payload data`returned by the node helper.
-   */
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "EXAMPLE_NOTIFICATION") {
-      this.templateContent = `${this.config.exampleContent} ${payload.text}`
-      this.updateDom()
-    }
-  },
+    // Define start sequence.
+    start: function() {
+        Log.info("Starting module: " + this.name);
 
-  /**
-   * Render the page we're on.
-   */
-  getDom() {
-    const wrapper = document.createElement("div")
-    wrapper.innerHTML = `<b>Title</b><br />${this.templateContent}`
+        this.sendConfig();
 
-    return wrapper
-  },
+        this.intervals = [];
+        this.alerts = [];
+        for (var i = 0; i < this.config.buttons.length; i++)
+        {
+            this.intervals.push(undefined);
+            this.alerts.push(false);
+        }
+    },
 
-  addRandomText() {
-    this.sendSocketNotification("GET_RANDOM_TEXT", { amountCharacters: 15 })
-  },
+    // Override dom generator.
+    getDom: function() {
+        var wrapper = document.createElement("div");
 
-  /**
-   * This is the place to receive notifications from other modules or the system.
-   *
-   * @param {string} notification The notification ID, it is preferred that it prefixes your module name
-   * @param {number} payload the payload type.
-   */
-  notificationReceived(notification, payload) {
-    if (notification === "TEMPLATE_RANDOM_TEXT") {
-      this.templateContent = `${this.config.exampleContent} ${payload}`
-      this.updateDom()
-    }
-  }
-})
+        return wrapper;
+    },
+
+    /* sendConfig()
+     * intialize backend
+     */
+    sendConfig: function() {
+        this.sendSocketNotification("BUTTON_CONFIG", {
+            config: this.config
+        });
+    },
+
+    buttonUp: function(index, duration) {
+        if (this.alerts[index]) {
+            // alert already shown, clear interval to update it and hide it
+            if (this.intervals[index] !== undefined) {
+                clearInterval(this.intervals[index]);
+            }
+            this.alerts[index] = false;
+            this.sendNotification("HIDE_ALERT");
+        } else {
+            // no alert shown, clear time out for showing it
+            if (this.intervals[index] !== undefined) {
+                clearTimeout(this.intervals[index]);
+            }
+        }
+        this.intervals[index] = undefined;
+
+        var min = this.config.minPressTime;
+        var max = this.config.maxPressTime;
+        var press = this.config.buttons[index].press
+        
+        if (press && min <= duration && duration <= max)
+        {
+            this.sendAction(press);
+        }
+    },
+
+    sendAction: function(description) {
+        this.sendNotification(description.notification, description.payload);
+    },
+
+    buttonDown: function(index) {
+        var self = this;
+
+        if (self.config.buttons[index].longPress && self.config.buttons[index].longPress.title)
+        {
+            this.intervals[index] = setTimeout(function () {
+                self.startAlert(index);
+            }, this.config.maxPressTime);
+        }
+    },
+
+    showAlert: function (index) {
+        // display the message
+        this.sendNotification("SHOW_ALERT", {
+            title: this.config.buttons[index].longPress.title,
+            message: this.config.buttons[index].longPress.message,
+            imageFA: this.config.buttons[index].longPress.imageFA
+        });
+    },
+
+    startAlert: function(index) {
+        this.alerts[index] = true;
+        this.showAlert(index);
+    },
+
+    // Override socket notification handler.
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "BUTTON_UP")
+        {
+            this.buttonUp(payload.index, payload.duration);
+        }
+        if (notification === "BUTTON_DOWN")
+        {
+            this.buttonDown(payload.index);
+        }
+    },
+});
